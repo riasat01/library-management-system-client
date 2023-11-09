cc23eac37979790e3623f73bd2d66cb01bd9729f
unknown <riasat01@outlook.com>
2023-11-09 19:51:12 +0600

    deployed

--- src/custom-hooks/useAxiosSecure.jsx
+++ src/custom-hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import swal from 'sweetalert';
 
 
 const AxiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://library-management-system-backend-vert.vercel.app',
     withCredentials: true
 })
 
